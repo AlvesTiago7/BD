1c26ad53007b36a25b49e972d7430fb28ac89860
Tiago <tiago.tiaguinho2013@hotmail.com>
2022-05-16 19:12:56 -0300

    Lição de BD

---
+++ biblioteca.sql
@@ -0,0 +1,39 @@
+create database livraria;
+
+create table pessoafisica(
+    cpf varchar(11) primary key not null
+);
+
+create table cliente(
+    codigo int primary key not null,
+    telefone varchar(11),
+    endereço varchar(45),
+    cpf varchar(11) primary key not null,
+    foreign key (cpf) references 
+    pessoafisica (cpf)
+);
+
+create table emprestimo (
+    codigocliente int not null,
+    isbn int not null,
+    dataemp date,
+    datadev date,
+    primary key (codigocliente, isbn)
+    );
+
+create table livro(
+    isbn int primary key not null,
+    qtd_estoque int,
+    assunto varchar(45),
+    autor varchar(45),
+    codigoeditora varchar(45) 
+
+);
+
+create table editora(
+    codigo int primary key not null,
+    endereco varchar(45),
+    telefone varchar(11),
+    gerente varchar(45)
+
+);
